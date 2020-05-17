fca48c87af82ded1dc3f669e3db8618da26c89b5
Abdulla Mufeeth <Abdulla.Mufeeth@emc.com>
2020-05-17 12:19:32 +0530

    Added aditional info in weather data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if(response.body.error){
             callback('Unable to find location', undefined);
         }else{
-            callback(undefined, response.body.current.weather_descriptions[0]+' It is currently '+response.body.current.temperature+' degrees out, It feels like '+response.body.current.feelslike+' degrees out');
+            callback(undefined, response.body.current.weather_descriptions[0]+' It is currently '+response.body.current.temperature+' degrees out, It feels like '+response.body.current.feelslike+' degrees out. The humidity is '+ response.body.current.humidity +' degree and wind speed is '+response.body.current.wind_speed);
         }
     })
 }
